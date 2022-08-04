0356b62e0a9a74931dc86ed6ee6a69eab6f3d563
gstelu <108110501+gstelu@users.noreply.github.com>
2022-08-04 10:27:10 +0530

    Add files via upload

---
+++ serveletsdemo1/src/main/java/com/example/serveletsdemo1/Serveletsdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.serveletsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serveletsdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serveletsdemo1Application.class, args);
+	}
+
+}
---
+++ serveletsdemo1/src/test/java/com/example/serveletsdemo1/Serveletsdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveletsdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serveletsdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
